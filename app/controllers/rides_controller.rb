e0b36dc86b549eb92c1a67f5d152a8d64c37d68c
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-17 16:09:47 -0700

    table showing rides

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,11 +4,9 @@ class RidesController < ApplicationController
 
   def index
     @rides = Ride.all
-    # @origin_rides = Ride.where(origin: ride_params)
   end
 
   def create
-    binding.pry
   end
   def ride_params
     params.require(:ride).permit(:origin, :destination)
