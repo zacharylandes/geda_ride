0166a273a392d2c45f4e54e1d47998ea71d83f1d
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-26 15:39:06 -0700

    google oauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-        params.require(:user).permit(:name, :image)
+    params.require(:user).permit(:name, :image)
   end
 
 end
