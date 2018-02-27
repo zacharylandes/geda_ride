c799f72dc50a42ba7d202f2c323dc32d67606896
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-26 21:51:35 -0700

    fog

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['S3_KEY'],
     :aws_secret_access_key  => ENV['S3_SECRET'],
-    :region                 => 'us-west-2'
+    :region                 => 'us-west-1'
   }
   config.fog_directory  = ENV['S3_BUCKET']
 end
