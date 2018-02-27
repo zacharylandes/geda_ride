6053b6a1cfa1d5a4dd087322cc13d6ad23d6a450
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-26 22:10:00 -0700

    figaro

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,8 @@ require 'carrierwave/orm/activerecord'
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV['S3_KEY'],
-    :aws_secret_access_key  => ENV['S3_SECRET'],
+    :aws_access_key_id      => ENV['aws_access_key_id'],
+    :aws_secret_access_key  => ENV['aws_secret_access_key'],
     :region                 => 'us-west-2'
   }
   config.fog_directory  = ENV['S3_BUCKET']
