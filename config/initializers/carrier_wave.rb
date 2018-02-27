406ec264b08f043a6f7ebb3dfaffffb49662d6c7
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-26 23:00:46 -0700

    envs

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ require 'carrierwave/orm/activerecord'
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AKIAJQSCFS5HS2WWHZ4A'],
-    :aws_secret_access_key  => ENV['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'],
+    :aws_access_key_id      => 'AKIAJQSCFS5HS2WWHZ4A',
+    :aws_secret_access_key  => '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',
     :region                 => 'us-west-2'
   }
   config.fog_directory  = ENV['S3_BUCKET']
