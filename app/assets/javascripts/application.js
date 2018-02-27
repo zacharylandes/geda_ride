33a2e377eb06c5d9a120dcae2bcdbbac33fda830
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-26 19:06:49 -0700

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,5 +9,6 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-
+//= require jquery
+//= require jquery_ui.min
 //= require_tree .
