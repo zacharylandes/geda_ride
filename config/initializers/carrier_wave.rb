91d0a82bee8bfd19f01c8ec277ced0fde7b41230
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-26 21:46:55 -0700

    fog

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -11,8 +11,8 @@ CarrierWave.configure do |config|
 
     # Configuration for Amazon S3
     :provider              => 'AWS',
-    :aws_access_key_id     => ENV['S3_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET'],
+    :aws_access_key_id     => ENV['aws_access_key_id'],
+    :aws_secret_access_key => ENV['aws_secret_access_key'],
     :region                => ENV['S3_REGION']
   }
 
