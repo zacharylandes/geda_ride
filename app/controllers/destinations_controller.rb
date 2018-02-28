5c4604d502d408216c2d4d97a280710a58418487
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-28 14:37:34 -0700

    cleanups

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,8 +1,9 @@
 class DestinationsController < ApplicationController
 
 def index
-    dests = Destination.find(params['format'])
-    @destinations = Destination.where(full_street_address: dests.full_street_address)
+    dest = Destination.find(params['format'])
+    @destinations = Destination.where(full_street_address: dest.full_street_address)
+    @events = EventService.new.find_event(dest.full_street_address)
 end
 
   def show
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,9 @@ CarrierWave.configure do |config|
   config.aws_acl    = 'public-read'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
   config.aws_credentials = {
-    access_key_id:     'AKIAIYKICS6OONP4SO5Q',
-    secret_access_key: '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',
+    access_key_id:     ENV['AW_KEY'],
+    secret_access_key: ENV['AW_SECRET'],
     endpoint:'https://s3-us-west-1.amazonaws.com/gedaride/',
-    region:            "us-west-2" # Required
+    region:            "us-west-2"
   }
 end
